6c18e6c13379d7c3ebc85b2cafd161c26463bfa6
kuvukilink <37421986+kuvukilink@users.noreply.github.com>
2018-03-27 05:05:23 +0700

    Update pemanggil-script.js

--- pemanggil-script.js
+++ pemanggil-script.js
@@ -21,9 +21,7 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*3)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2017/11/jeep-wrangler-redesigned-fiat-chrysler.html"}
-if(x=="2"){xxx="/2017/11/car-buyers-get-bigger-discounts-in.html"}
-if(x=="3"){xxx="/2017/11/how-to-choose-health-insurance-policy.html"}a_to_ve[i].href="http://www.kuvukilink.ml"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{a_to_ve[i].href="http://www.kuvukilink.ml"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
